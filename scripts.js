6dbdeeb1afa56df9cad77e81805f6da605fe40c7
isaqli <117958160+isaqli@users.noreply.github.com>
2025-07-21 02:06:13 +0000

    version 6

---
+++ scripts.js
@@ -0,0 +1,12 @@
+//tab functionality
+document.addEventListener('DOMContentLoaded', function() {
+    const tabBtns = document.querySelectorAll('.nav-link');
+    const tabContents = document.querySelectorAll('.tab-content');
+
+    tabBtns.forEach(btn => {
+        btn.addEventListener('click', function() {
+            // Remove active class from all buttons and contents
+            
+        })
+    })
+})
