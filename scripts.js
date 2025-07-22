bc2ab0ac0a1d31cf1683077690d71c203d7785ee
isaqli <117958160+isaqli@users.noreply.github.com>
2025-07-22 04:49:55 +0000

    version 9

--- scripts.js
+++ scripts.js
@@ -1,12 +1,23 @@
 //tab functionality
 document.addEventListener('DOMContentLoaded', function() {
-    const tabBtns = document.querySelectorAll('.nav-link');
+    const tabBtns = document.querySelectorAll('.navbar a');
     const tabContents = document.querySelectorAll('.tab-content');
 
-    tabBtns.forEach(btn => {
-        btn.addEventListener('click', function() {
-            // Remove active class from all buttons and contents
-            
-        })
-    })
+    function showTab(tabId) {
+        tabContents.forEach(tab => tab.classList.remove('active'));
+        navLinks.forEach(link => link.classList.remove('active'));
+                
+        document.getElementById(tabId).classList.add('active');
+        document.querySelector(`.navbar a[data-tab="${tabId}"]`).classList.add('active');
+    }
+    // Set default tab
+    showTab('beginner');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const tabId = this.getAttribute('data-tab');
+            showTab(tabId);
+        });
+    });
 })
